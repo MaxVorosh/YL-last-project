fa12c694ed46187080b2df54241bdee3fc36c132
Denittka <dan.belokon@gmail.com>
2020-04-20 15:49:54 +0300

    fix deals

--- data/Deals.py
+++ data/Deals.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, Text, Datetime
+from sqlalchemy import Column, Integer, String, ForeignKey, Text, DateTime
 from sqlalchemy_serializer import SerializerMixin
 from .db_session import SqlAlchemyBase
 
@@ -9,4 +9,4 @@ class Deal(SqlAlchemyBase, SerializerMixin):
     product = Column(Integer, ForeignKey("products.id"))
     participants = Column(String)
     history = Column(Text, nullable=True)
-    date = Column(Datetime)
+    date = Column(DateTime)
